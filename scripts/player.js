9736d78c57c704e094c140fcd3fc8e73b4b69ab0
Angela Montanez <angelamountains@gmail.com>
2024-05-26 15:01:31 +0200

    dynamic player resizing with the ratio of canvas height

--- scripts/player.js
+++ scripts/player.js
@@ -3,13 +3,19 @@ class Player {
     this.game = game;
     this.x = 50;
     this.y = 60;
-    this.width = 200;
-    this.height = 200;
+    this.spriteWidth = 200; //Sprite png given for the tutorial is 200px by 200px
+    this.spriteHeight = 200;
+    this.width;
+    this.height;
   }
   draw() {
     this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
   }
   update() {
-    this.x++;
+    // this.x++;
+  }
+  resize() {
+    this.width = 200;
+    this.height = 200;
   }
 }
