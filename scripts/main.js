0049e8c700cb9825e9562d971c077673e2958414
Angela Montanez <angelamountains@gmail.com>
2024-05-26 14:00:06 +0200

    player animation and html canvas, js, and css starter set up

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+class Game {
+  constructor(canvas, context) {
+    this.canvas = canvas;
+    this.ctx = context;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.player = new Player(this);
+  }
+  render() {
+    this.ctx.fillStyle = "red";
+    this.player.update();
+    this.player.draw();
+  }
+}
+
+window.addEventListener("load", function () {
+  const canvas = this.document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 720;
+  canvas.height = 720;
+
+  const game = new Game(canvas, ctx);
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.render();
+    requestAnimationFrame(animate);
+  }
+  this.requestAnimationFrame(animate);
+});
